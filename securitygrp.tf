c820cbca7379e41c612cd3906397b513fcf9b42f
Jaladeesh <67995555+jaladeesh@users.noreply.github.com>
2024-09-13 12:16:25 +0530

    added s3 bucket configuration

--- securitygrp.tf
+++ securitygrp.tf
@@ -4,6 +4,15 @@ provider "aws" {
 region = "ap-south-1"
 }
 
+resource "aws_s3_bucket" "example13092024" {
+  bucket = "my-tf-test-bucket13092024"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
 resource "aws_security_group" "http-sg" {
   ingress {
     from_port   = 80
